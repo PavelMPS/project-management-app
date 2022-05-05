9c3fa72fa9119f1482018e9340c5727e4e2e151a
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-05-05 15:00:53 +0300

    feat: add application basic structure

---
+++ src/components/app/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Aside from '../aside/Aside';
+import Footer from '../footer-page/Footer';
+import Header from '../header/Header';
+import Main from '../main-page/Main';
+import NotFound from '../not-found-page/NotFound';
+import Welcome from '../welcome-page/Welcome';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Aside />
+      <div className="main-container">
+        <Routes>
+          <Route path="/" element={<Welcome />}/>
+          <Route path="/*" element={<NotFound />}/>
+          <Route path="/main" element={<Main />}/>
+        </Routes>
+      </div>
+      <Footer />
+    </>
+    
+  );
+}
+
+export default App;
