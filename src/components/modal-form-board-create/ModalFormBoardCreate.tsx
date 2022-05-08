dad853f4ab596cf6c2fb280c0117bb5e6c984947
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-05-08 13:14:49 +0300

    fix: remove description from board popup

--- src/components/modal-form-board-create/ModalFormBoardCreate.tsx
+++ src/components/modal-form-board-create/ModalFormBoardCreate.tsx
@@ -8,11 +8,9 @@ import './modalFormBoardCreate.css';
 const ModalFormBoardCreate = (): JSX.Element => {
   const [title, setTitle] = useState('');
   const dispatch = useDispatch<AppDispatch>();
-
   function createBoardHandler(e: FormEvent) {
     e.preventDefault();
     dispatch(createBoard(title));
-    console.log('create board data ' + title);
   }
 
   return (
@@ -20,23 +18,13 @@ const ModalFormBoardCreate = (): JSX.Element => {
       <h2 className="modal-board-title">Create board</h2>
       <form className="modal-board-form">
         <label className="form-label board-label">
-          <p className="title-label">
-            Board title:
-            {/* <span className="errors">{errors[label] && errMess}</span> */}
-          </p>
+          <p className="title-label">Board title:</p>
           <input
             onChange={(e) => setTitle(e.target.value)}
             type="text"
             placeholder="Enter board title"
             value={title}
           />
-        </label>
-        <label className="form-label board-label">
-          <p className="title-label">
-            Description:
-            {/* <span className="errors">{errors[label] && errMess}</span> */}
-          </p>
-          <input type="text" placeholder="Enter board description" />
         </label>
         <button className="board-create-btn" onClick={createBoardHandler}>
           CREATE
