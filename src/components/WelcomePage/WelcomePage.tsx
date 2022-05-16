c83e4a51cbc47431bad54c26bea15cff5b02a207
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2022-05-16 20:26:42 +0300

    feat: add authorization logic

--- src/components/WelcomePage/WelcomePage.tsx
+++ src/components/WelcomePage/WelcomePage.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect } from 'react';
+
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks/redux';
+
 import { buttonName, pageName } from '../../constants/Constants';
+import { setToken } from '../../redux/userSlice';
 
 const WelcomePage = (): JSX.Element => {
+  const navigate = useNavigate();
+  const { token, isAuth } = useAppSelector((store) => store.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    userAuth();
+    if (token && isAuth) {
+      return navigate('/main');
+    } else {
+      return navigate('/');
+    }
+  }, [isAuth]);
+
+  const userAuth = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(setToken(token));
+    }
+  };
+
   return (
     <>
       <h1>{pageName.welcome}</h1>
